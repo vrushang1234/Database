91b25e6907d925fc791d89b4ec081b3426fc8882
vrushang1234 <vrushanganand20@gmail.com>
2024-07-04 09:50:45 -0700

    Initial Commit

---
+++ client.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#include <thread>
+#include <atomic>
+
+#define PORT 8080
+
+std::atomic<bool> running(true);
+
+void receiveMessages(int sock)
+{
+    char buffer[1024] = {0};
+    while (running)
+    {
+        int valread = read(sock, buffer, 1024);
+        if (valread > 0)
+        {
+            std::cout << "Server: " << buffer << "\n";
+            memset(buffer, 0, sizeof(buffer));
+        }
+        else if (valread == 0)
+        {
+            std::cout << "Server closed the connection\n";
+            close(sock);
+            running = false;
+            break;
+        }
+        else
+        {
+            std::cerr << "Read error\n";
+            close(sock);
+            running = false;
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        std::cerr << "Socket creation failed\n";
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
+        std::cerr << "Invalid address/ Address not supported\n";
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        std::cerr << "Connection Failed\n";
+        return -1;
+    }
+
+    std::cout << "Connected to the server\n";
+
+    std::thread recvThread(receiveMessages, sock);
+    recvThread.detach();
+
+    while (running)
+    {
+        std::string message;
+        std::getline(std::cin, message);
+
+        if (message == "exit")
+        {
+            std::cout << "Exiting...\n";
+            running = false;
+            break;
+        }
+
+        if (send(sock, message.c_str(), message.size(), 0) < 0)
+        {
+            std::cerr << "Send failed\n";
+            running = false;
+            break;
+        }
+    }
+
+    close(sock);
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+#include <thread>
+#include <unordered_map>
+#include <sstream>
+#include <atomic>
+
+#define PORT 8080
+
+std::unordered_map<std::string, std::string> data_store;
+std::atomic<bool> running(true);
+
+void handleClient(int client_socket)
+{
+    char buffer[1024] = {0};
+
+    while (running)
+    {
+        int valread = read(client_socket, buffer, 1024);
+        if (valread > 0)
+        {
+            std::string message(buffer);
+            std::stringstream ss(message);
+            std::string command, key, value;
+
+            ss >> command >> key;
+            if (command == "SET")
+            {
+                ss >> value;
+                data_store[key] = value;
+                std::string response = "SET OK\n";
+                send(client_socket, response.c_str(), response.size(), 0);
+            }
+            else if (command == "GET")
+            {
+                auto it = data_store.find(key);
+                if (it != data_store.end())
+                {
+                    std::string response = "VALUE: " + it->second + "\n";
+                    send(client_socket, response.c_str(), response.size(), 0);
+                }
+                else
+                {
+                    std::string response = "KEY NOT FOUND\n";
+                    send(client_socket, response.c_str(), response.size(), 0);
+                }
+            }
+            else
+            {
+                std::string response = "UNKNOWN COMMAND\n";
+                send(client_socket, response.c_str(), response.size(), 0);
+            }
+            memset(buffer, 0, sizeof(buffer));
+        }
+        else if (valread == 0)
+        {
+            std::cout << "Client disconnected\n";
+            break;
+        }
+        else
+        {
+            std::cerr << "Read error\n";
+            break;
+        }
+    }
+
+    close(client_socket);
+}
+
+int main()
+{
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        std::cerr << "Socket creation failed\n";
+        return -1;
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
+        std::cerr << "Setsockopt failed\n";
+        return -1;
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        std::cerr << "Bind failed\n";
+        return -1;
+    }
+
+    if (listen(server_fd, 3) < 0)
+    {
+        std::cerr << "Listen failed\n";
+        return -1;
+    }
+
+    std::cout << "Server listening on port " << PORT << "\n";
+
+    while (true)
+    {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        {
+            std::cerr << "Accept failed\n";
+            continue;
+        }
+
+        std::thread client_thread(handleClient, new_socket);
+        client_thread.detach();
+    }
+
+    return 0;
+}
